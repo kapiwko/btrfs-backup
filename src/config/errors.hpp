be86cb3e48b0c47d377550a36aa6b89bf63966b4
Kamil Piwowarski <kapiwko@gmail.com>
2026-08-26 11:24:51 +0200

    refactor(core): organize exception hierarchy

--- src/config/errors.hpp
+++ src/config/errors.hpp
@@ -10,24 +10,51 @@
 
 namespace btrfsbackup {
 
-struct ValidationError : std::runtime_error {
+struct BtrfsBackupError : std::runtime_error {
     using std::runtime_error::runtime_error;
 };
 
-struct CodedValidationError : ValidationError {
-    CodedValidationError(std::string error_code, const std::string& message)
-        : ValidationError(message), error_code(std::move(error_code)) {
+struct ConfigurationError : BtrfsBackupError {
+    using BtrfsBackupError::BtrfsBackupError;
+};
+
+struct ValidationError : ConfigurationError {
+    using ConfigurationError::ConfigurationError;
+};
+
+struct SystemOperationError : BtrfsBackupError {
+    using BtrfsBackupError::BtrfsBackupError;
+};
+
+struct CodedError {
+    explicit CodedError(std::string error_code) : error_code(std::move(error_code)) {
     }
+
+    virtual ~CodedError() = default;
 
     std::string error_code;
 };
 
-struct RecoveryRequiredError : CodedValidationError {
-    using CodedValidationError::CodedValidationError;
+struct CodedValidationError : ValidationError, CodedError {
+    CodedValidationError(std::string error_code, const std::string& message)
+        : ValidationError(message), CodedError(std::move(error_code)) {
+    }
 };
 
-struct OperationCancelledError : ValidationError {
-    using ValidationError::ValidationError;
+struct CodedOperationError : SystemOperationError, CodedError {
+    CodedOperationError(std::string error_code, const std::string& message)
+        : SystemOperationError(message), CodedError(std::move(error_code)) {
+    }
+};
+
+struct RecoveryRequiredError : BtrfsBackupError, CodedError {
+    RecoveryRequiredError(std::string error_code, const std::string& message)
+        : BtrfsBackupError(message), CodedError(std::move(error_code)) {
+    }
+};
+
+struct OperationCancelledError : BtrfsBackupError {
+    using BtrfsBackupError::BtrfsBackupError;
 };
 
 } // namespace btrfsbackup
