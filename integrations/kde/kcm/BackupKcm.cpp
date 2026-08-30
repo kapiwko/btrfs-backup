d8c6af0624df888c391ab8ab933b41cc1538f28c
Kamil Piwowarski <9luty1992@gmail.com>
2026-08-31 00:10:46 +0200

    feat(kcm): add backup overview and target validation

---
+++ integrations/kde/kcm/BackupKcm.cpp
@@ -0,0 +1,42 @@
+// SPDX-FileCopyrightText: 2026 Kamil Piwowarski <kapiwko@gmail.com>
+//
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+#include "BackupKcm.hpp"
+
+#include <KPluginFactory>
+
+#include <QDesktopServices>
+#include <QProcess>
+#include <QUrl>
+
+namespace btrfsbackup::kde::kcm {
+
+BackupKcm::BackupKcm(QObject* parent, const KPluginMetaData& metadata)
+    : KQuickConfigModule(parent, metadata) {
+    setButtons(NoAdditionalButton);
+}
+
+void BackupKcm::openSystemLog() {
+    QProcess::startDetached(
+        QStringLiteral("konsole"),
+        {
+            QStringLiteral("--hold"),
+            QStringLiteral("-e"),
+            QStringLiteral("journalctl"),
+            QStringLiteral("-u"),
+            QStringLiteral("btrfs-backupd.service"),
+            QStringLiteral("--no-pager"),
+        }
+    );
+}
+
+void BackupKcm::openSupportPage() {
+    QDesktopServices::openUrl(QUrl(QStringLiteral("https://github.com/kapiwko/btrfs-backup/issues")));
+}
+
+} // namespace btrfsbackup::kde::kcm
+
+K_PLUGIN_CLASS_WITH_JSON(btrfsbackup::kde::kcm::BackupKcm, "kcm_btrfsbackup.json")
+
+#include "BackupKcm.moc"
