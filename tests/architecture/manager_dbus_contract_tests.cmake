9943566ce2bbfcfa4afc133a4f4d262a33005c08
Kamil Piwowarski <kapiwko@gmail.com>
2026-09-04 00:14:18 +0200

    test(dbus): enforce complete server and KDE contract parity

--- tests/architecture/manager_dbus_contract_tests.cmake
+++ tests/architecture/manager_dbus_contract_tests.cmake
@@ -16,6 +16,17 @@ file(READ "${PROJECT_SOURCE_DIR}/integrations/kde/kcm/TargetCredentialModel.cpp"
 file(READ "${PROJECT_SOURCE_DIR}/integrations/kde/kcm/ui/ProfileOverview.qml" profile_overview_kcm)
 file(READ "${PROJECT_SOURCE_DIR}/integrations/kde/models/ProfilePresentation.qml" profile_presentation)
 file(READ "${PROJECT_SOURCE_DIR}/integrations/kde/models/BackupStatusModel.cpp" plasma_status_model)
+file(GLOB_RECURSE kde_dbus_sources
+    "${PROJECT_SOURCE_DIR}/integrations/kde/*.cpp"
+    "${PROJECT_SOURCE_DIR}/integrations/kde/*.hpp"
+    "${PROJECT_SOURCE_DIR}/integrations/kde/*.qml"
+)
+list(FILTER kde_dbus_sources EXCLUDE REGEX "/tests/")
+set(kde_dbus_contract)
+foreach(source IN LISTS kde_dbus_sources)
+    file(READ "${source}" content)
+    string(APPEND kde_dbus_contract "${content}\n")
+endforeach()
 string(REGEX REPLACE "[ \t\r\n]+" "" compact_xml "${manager_xml}")
 string(REGEX REPLACE "[ \t\r\n]+" "" compact_bus_policy "${manager_bus_policy}")
 string(REGEX REPLACE "[ \t\r\n]+" "" compact_polkit_policy "${manager_polkit_policy}")
@@ -40,8 +51,26 @@ function(assert_not_contains content fragment description)
     endif()
 endfunction()
 
+function(assert_same_set actual_variable expected_variable description)
+    set(actual ${${actual_variable}})
+    set(expected ${${expected_variable}})
+    list(REMOVE_DUPLICATES actual)
+    list(REMOVE_DUPLICATES expected)
+    list(SORT actual)
+    list(SORT expected)
+    if(NOT "${actual}" STREQUAL "${expected}")
+        message(FATAL_ERROR
+            "manager D-Bus contract has inconsistent ${description}\n"
+            "actual: ${actual}\n"
+            "expected: ${expected}"
+        )
+    endif()
+endfunction()
+
 function(assert_authorized_method identifier name input_signature output_signature xml_fragment action action_id)
     assert_method(${identifier} ${name} "${input_signature}" "${output_signature}" "${xml_fragment}")
+    set_property(GLOBAL APPEND PROPERTY manager_authorized_method_ids "${identifier}")
+    set_property(GLOBAL APPEND PROPERTY manager_authorization_action_ids "${action_id}")
     assert_contains(
         "${compact_authorization_map}"
         "{manager_protocol::method::${identifier},ManagerAuthorizationAction::${action}}"
@@ -61,6 +90,7 @@ endfunction()
 
 function(assert_unprivileged_method identifier name input_signature output_signature xml_fragment)
     assert_method(${identifier} ${name} "${input_signature}" "${output_signature}" "${xml_fragment}")
+    set_property(GLOBAL APPEND PROPERTY manager_unprivileged_method_ids "${identifier}")
     assert_not_contains(
         "${compact_authorization_map}"
         "{manager_protocol::method::${identifier},"
@@ -69,6 +99,8 @@ function(assert_unprivileged_method identifier name input_signature output_signa
 endfunction()
 
 function(assert_method identifier name input_signature output_signature xml_fragment)
+    set_property(GLOBAL APPEND PROPERTY manager_contract_method_ids "${identifier}")
+    set_property(GLOBAL APPEND PROPERTY manager_contract_method_names "${name}")
     assert_contains(
         "${manager_protocol}"
         "inline constexpr char ${identifier}[] = \"${name}\";"
@@ -88,6 +120,8 @@ function(assert_method identifier name input_signature output_signature xml_frag
 endfunction()
 
 function(assert_signal identifier name signature xml_fragment)
+    set_property(GLOBAL APPEND PROPERTY manager_contract_signal_ids "${identifier}")
+    set_property(GLOBAL APPEND PROPERTY manager_contract_signal_names "${name}")
     assert_contains(
         "${manager_protocol}"
         "inline constexpr char ${identifier}[] = \"${name}\";"
@@ -151,103 +185,114 @@ assert_contains(
     "the manager object path and interface"
 )
 
-assert_method(
+assert_unprivileged_method(
     get_capabilities GetCapabilities "" s
     "<methodname=\"GetCapabilities\"><argname=\"payload\"type=\"s\"direction=\"out\"/></method>"
 )
-assert_method(
+assert_unprivileged_method(
     list_profiles ListProfiles "" s
     "<methodname=\"ListProfiles\"><argname=\"payload\"type=\"s\"direction=\"out\"/></method>"
 )
-assert_method(
+assert_unprivileged_method(
     get_status GetStatus s s
     "<methodname=\"GetStatus\"><argname=\"profileId\"type=\"s\"direction=\"in\"/><argname=\"payload\"type=\"s\"direction=\"out\"/></method>"
 )
-assert_method(
+assert_unprivileged_method(
     get_history_sanitized GetHistorySanitized suu s
     "<methodname=\"GetHistorySanitized\"><argname=\"profileId\"type=\"s\"direction=\"in\"/><argname=\"offset\"type=\"u\"direction=\"in\"/><argname=\"limit\"type=\"u\"direction=\"in\"/><argname=\"payload\"type=\"s\"direction=\"out\"/></method>"
 )
-assert_method(
+assert_unprivileged_method(
     get_device_state GetDeviceState s s
     "<methodname=\"GetDeviceState\"><argname=\"profileId\"type=\"s\"direction=\"in\"/><argname=\"payload\"type=\"s\"direction=\"out\"/></method>"
 )
-assert_method(
+assert_authorized_method(
     start_backup StartBackup s s
     "<methodname=\"StartBackup\"><argname=\"profileId\"type=\"s\"direction=\"in\"/><argname=\"payload\"type=\"s\"direction=\"out\"/></method>"
+    StartBackup io.github.btrfsbackup.start-backup
 )
-assert_method(
+assert_authorized_method(
     cancel_backup CancelBackup ss s
     "<methodname=\"CancelBackup\"><argname=\"profileId\"type=\"s\"direction=\"in\"/><argname=\"runId\"type=\"s\"direction=\"in\"/><argname=\"payload\"type=\"s\"direction=\"out\"/></method>"
+    CancelBackup io.github.btrfsbackup.cancel-backup
 )
-assert_method(
+assert_authorized_method(
     validate_target ValidateTarget s s
     "<methodname=\"ValidateTarget\"><argname=\"profileId\"type=\"s\"direction=\"in\"/><argname=\"payload\"type=\"s\"direction=\"out\"/></method>"
+    ValidateTarget io.github.btrfsbackup.validate-target
 )
-assert_method(
+assert_authorized_method(
     eject_target EjectTarget s s
     "<methodname=\"EjectTarget\"><argname=\"profileId\"type=\"s\"direction=\"in\"/><argname=\"payload\"type=\"s\"direction=\"out\"/></method>"
+    EjectTarget io.github.btrfsbackup.eject-target
 )
-assert_method(
+assert_unprivileged_method(
     get_profile_details GetProfileDetails s s
     "<methodname=\"GetProfileDetails\"><argname=\"profileId\"type=\"s\"direction=\"in\"/><argname=\"payload\"type=\"s\"direction=\"out\"/></method>"
 )
-assert_method(
+assert_authorized_method(
     update_profile_settings UpdateProfileSettings ssss s
     "<methodname=\"UpdateProfileSettings\"><argname=\"profileId\"type=\"s\"direction=\"in\"/><argname=\"expectedGeneration\"type=\"s\"direction=\"in\"/><argname=\"expectedFingerprint\"type=\"s\"direction=\"in\"/><argname=\"request\"type=\"s\"direction=\"in\"/><argname=\"payload\"type=\"s\"direction=\"out\"/></method>"
+    ManageProfileConfiguration io.github.btrfsbackup.manage-profile-configuration
 )
-assert_method(
+assert_authorized_method(
     add_profile_source AddProfileSource ssss s
     "<methodname=\"AddProfileSource\"><argname=\"profileId\"type=\"s\"direction=\"in\"/><argname=\"expectedGeneration\"type=\"s\"direction=\"in\"/><argname=\"expectedFingerprint\"type=\"s\"direction=\"in\"/><argname=\"request\"type=\"s\"direction=\"in\"/><argname=\"payload\"type=\"s\"direction=\"out\"/></method>"
+    ManageProfileConfiguration io.github.btrfsbackup.manage-profile-configuration
 )
-assert_method(
+assert_authorized_method(
     update_profile_source UpdateProfileSource sssss s
     "<methodname=\"UpdateProfileSource\"><argname=\"profileId\"type=\"s\"direction=\"in\"/><argname=\"sourceId\"type=\"s\"direction=\"in\"/><argname=\"expectedGeneration\"type=\"s\"direction=\"in\"/><argname=\"expectedFingerprint\"type=\"s\"direction=\"in\"/><argname=\"request\"type=\"s\"direction=\"in\"/><argname=\"payload\"type=\"s\"direction=\"out\"/></method>"
+    ManageProfileConfiguration io.github.btrfsbackup.manage-profile-configuration
 )
-assert_method(
+assert_authorized_method(
     remove_profile_source RemoveProfileSource ssss s
     "<methodname=\"RemoveProfileSource\"><argname=\"profileId\"type=\"s\"direction=\"in\"/><argname=\"sourceId\"type=\"s\"direction=\"in\"/><argname=\"expectedGeneration\"type=\"s\"direction=\"in\"/><argname=\"expectedFingerprint\"type=\"s\"direction=\"in\"/><argname=\"payload\"type=\"s\"direction=\"out\"/></method>"
+    ManageProfileConfiguration io.github.btrfsbackup.manage-profile-configuration
 )
-assert_method(
+assert_authorized_method(
     delete_profile DeleteProfile sss s
     "<methodname=\"DeleteProfile\"><argname=\"profileId\"type=\"s\"direction=\"in\"/><argname=\"expectedGeneration\"type=\"s\"direction=\"in\"/><argname=\"expectedFingerprint\"type=\"s\"direction=\"in\"/><argname=\"payload\"type=\"s\"direction=\"out\"/></method>"
+    DeleteProfileConfiguration io.github.btrfsbackup.delete-profile-configuration
 )
-assert_method(
+assert_authorized_method(
     set_profile_enabled SetProfileEnabled sb s
     "<methodname=\"SetProfileEnabled\"><argname=\"profileId\"type=\"s\"direction=\"in\"/><argname=\"enabled\"type=\"b\"direction=\"in\"/><argname=\"payload\"type=\"s\"direction=\"out\"/></method>"
+    SetProfileEnabled io.github.btrfsbackup.set-profile-enabled
 )
-assert_method(
+assert_authorized_method(
     open_browse_session OpenBrowseSession s s
     "<methodname=\"OpenBrowseSession\"><argname=\"profileId\"type=\"s\"direction=\"in\"/><argname=\"payload\"type=\"s\"direction=\"out\"/></method>"
+    OpenBrowseSession io.github.btrfsbackup.open-browse-session
 )
-assert_method(
+assert_unprivileged_method(
     renew_browse_session RenewBrowseSession s s
     "<methodname=\"RenewBrowseSession\"><argname=\"sessionId\"type=\"s\"direction=\"in\"/><argname=\"payload\"type=\"s\"direction=\"out\"/></method>"
 )
-assert_method(
+assert_unprivileged_method(
     set_browse_session_active SetBrowseSessionActive sb s
     "<methodname=\"SetBrowseSessionActive\"><argname=\"sessionId\"type=\"s\"direction=\"in\"/><argname=\"active\"type=\"b\"direction=\"in\"/><argname=\"payload\"type=\"s\"direction=\"out\"/></method>"
 )
-assert_method(
+assert_unprivileged_method(
     close_browse_session CloseBrowseSession s s
     "<methodname=\"CloseBrowseSession\"><argname=\"sessionId\"type=\"s\"direction=\"in\"/><argname=\"payload\"type=\"s\"direction=\"out\"/></method>"
 )
-assert_method(
+assert_unprivileged_method(
     list_browse_directory ListBrowseDirectory ss s
     "<methodname=\"ListBrowseDirectory\"><argname=\"sessionId\"type=\"s\"direction=\"in\"/><argname=\"relativePath\"type=\"s\"direction=\"in\"/><argname=\"payload\"type=\"s\"direction=\"out\"/></method>"
 )
-assert_method(
+assert_unprivileged_method(
     inspect_browse_entry InspectBrowseEntry ss s
     "<methodname=\"InspectBrowseEntry\"><argname=\"sessionId\"type=\"s\"direction=\"in\"/><argname=\"relativePath\"type=\"s\"direction=\"in\"/><argname=\"payload\"type=\"s\"direction=\"out\"/></method>"
 )
-assert_method(
+assert_unprivileged_method(
     open_browse_file OpenBrowseFile ss h
     "<methodname=\"OpenBrowseFile\"><argname=\"sessionId\"type=\"s\"direction=\"in\"/><argname=\"relativePath\"type=\"s\"direction=\"in\"/><argname=\"file\"type=\"h\"direction=\"out\"/></method>"
 )
-assert_method(
+assert_unprivileged_method(
     inspect_browse_repository InspectBrowseRepository s s
     "<methodname=\"InspectBrowseRepository\"><argname=\"sessionId\"type=\"s\"direction=\"in\"/><argname=\"payload\"type=\"s\"direction=\"out\"/></method>"
 )
-assert_method(
+assert_unprivileged_method(
     resolve_backup_coverage ResolveBackupCoverage s s
     "<methodname=\"ResolveBackupCoverage\"><argname=\"localPath\"type=\"s\"direction=\"in\"/><argname=\"payload\"type=\"s\"direction=\"out\"/></method>"
 )
@@ -377,14 +422,134 @@ assert_signal(
     "<signalname=\"DeviceStateChanged\"><argname=\"profileId\"type=\"s\"/></signal>"
 )
 
-string(REGEX MATCHALL "<method name=" xml_methods "${manager_xml}")
-list(LENGTH xml_methods method_count)
-if(NOT method_count EQUAL 37)
-    message(FATAL_ERROR "manager XML must declare exactly 37 methods, found ${method_count}")
-endif()
+get_property(contract_method_ids GLOBAL PROPERTY manager_contract_method_ids)
+get_property(contract_method_names GLOBAL PROPERTY manager_contract_method_names)
+get_property(contract_signal_ids GLOBAL PROPERTY manager_contract_signal_ids)
+get_property(contract_signal_names GLOBAL PROPERTY manager_contract_signal_names)
+get_property(authorized_method_ids GLOBAL PROPERTY manager_authorized_method_ids)
+get_property(unprivileged_method_ids GLOBAL PROPERTY manager_unprivileged_method_ids)
+get_property(contract_action_ids GLOBAL PROPERTY manager_authorization_action_ids)
+set(classified_method_ids ${authorized_method_ids} ${unprivileged_method_ids})
+assert_same_set(classified_method_ids contract_method_ids "method authorization classifications")
 
-string(REGEX MATCHALL "<signal name=" xml_signals "${manager_xml}")
-list(LENGTH xml_signals signal_count)
-if(NOT signal_count EQUAL 4)
-    message(FATAL_ERROR "manager XML must declare exactly 4 signals, found ${signal_count}")
-endif()
+string(REGEX MATCHALL
+    "SD_BUS_METHOD\\(manager_protocol::method::[a-z0-9_]+"
+    vtable_method_entries
+    "${manager_vtable}"
+)
+set(vtable_method_ids)
+foreach(entry IN LISTS vtable_method_entries)
+    string(REGEX REPLACE ".*method::([a-z0-9_]+)" "\\1" identifier "${entry}")
+    list(APPEND vtable_method_ids "${identifier}")
+endforeach()
+assert_same_set(vtable_method_ids contract_method_ids "daemon vtable methods")
+
+string(REGEX MATCHALL "<methodname=\"[A-Za-z0-9]+\"" xml_method_entries "${compact_xml}")
+set(xml_method_names)
+foreach(entry IN LISTS xml_method_entries)
+    string(REGEX REPLACE ".*=\"([A-Za-z0-9]+)\"" "\\1" name "${entry}")
+    list(APPEND xml_method_names "${name}")
+endforeach()
+assert_same_set(xml_method_names contract_method_names "XML methods")
+
+string(REGEX MATCHALL
+    "send_interface=\"io.github.btrfsbackup.Manager1\"send_member=\"[A-Za-z0-9]+\""
+    bus_policy_method_entries
+    "${compact_bus_policy}"
+)
+set(bus_policy_method_names)
+foreach(entry IN LISTS bus_policy_method_entries)
+    string(REGEX REPLACE ".*send_member=\"([A-Za-z0-9]+)\"" "\\1" name "${entry}")
+    list(APPEND bus_policy_method_names "${name}")
+endforeach()
+assert_same_set(bus_policy_method_names contract_method_names "system bus policy methods")
+
+string(REGEX MATCHALL
+    "\\{manager_protocol::method::[a-z0-9_]+,ManagerAuthorizationAction::"
+    authorization_method_entries
+    "${compact_authorization_map}"
+)
+set(authorization_method_ids)
+foreach(entry IN LISTS authorization_method_entries)
+    string(REGEX REPLACE ".*method::([a-z0-9_]+),.*" "\\1" identifier "${entry}")
+    list(APPEND authorization_method_ids "${identifier}")
+endforeach()
+assert_same_set(authorization_method_ids authorized_method_ids "shared authorization mappings")
+
+string(REGEX MATCHALL "<actionid=\"[a-z0-9.-]+\"" polkit_action_entries "${compact_polkit_policy}")
+set(polkit_action_ids)
+foreach(entry IN LISTS polkit_action_entries)
+    string(REGEX REPLACE ".*=\"([a-z0-9.-]+)\"" "\\1" action_id "${entry}")
+    list(APPEND polkit_action_ids "${action_id}")
+endforeach()
+string(REGEX MATCHALL
+    "caseManagerAuthorizationAction::[A-Za-z]+:return\"[a-z0-9.-]+\";"
+    mapped_action_entries
+    "${compact_authorization_map}"
+)
+set(mapped_action_ids)
+foreach(entry IN LISTS mapped_action_entries)
+    string(REGEX REPLACE ".*return\"" "" action_id "${entry}")
+    string(REGEX REPLACE "\".*" "" action_id "${action_id}")
+    list(APPEND mapped_action_ids "${action_id}")
+endforeach()
+assert_same_set(polkit_action_ids mapped_action_ids "installed and mapped polkit actions")
+assert_same_set(polkit_action_ids contract_action_ids "method and polkit action mappings")
+
+string(REGEX MATCHALL
+    "manager_protocol::method::[a-z0-9_]+"
+    kde_method_entries
+    "${kde_dbus_contract}"
+)
+set(kde_method_ids)
+foreach(entry IN LISTS kde_method_entries)
+    string(REGEX REPLACE ".*method::([a-z0-9_]+)" "\\1" identifier "${entry}")
+    list(APPEND kde_method_ids "${identifier}")
+endforeach()
+assert_same_set(kde_method_ids contract_method_ids "KDE client methods")
+
+string(REGEX MATCHALL
+    "SD_BUS_SIGNAL\\(manager_protocol::signal::[a-z0-9_]+"
+    vtable_signal_entries
+    "${manager_vtable}"
+)
+set(vtable_signal_ids)
+foreach(entry IN LISTS vtable_signal_entries)
+    string(REGEX REPLACE ".*signal::([a-z0-9_]+)" "\\1" identifier "${entry}")
+    list(APPEND vtable_signal_ids "${identifier}")
+endforeach()
+assert_same_set(vtable_signal_ids contract_signal_ids "daemon vtable signals")
+
+string(REGEX MATCHALL "<signalname=\"[A-Za-z0-9]+\"" xml_signal_entries "${compact_xml}")
+set(xml_signal_names)
+foreach(entry IN LISTS xml_signal_entries)
+    string(REGEX REPLACE ".*=\"([A-Za-z0-9]+)\"" "\\1" name "${entry}")
+    list(APPEND xml_signal_names "${name}")
+endforeach()
+assert_same_set(xml_signal_names contract_signal_names "XML signals")
+
+string(REGEX MATCHALL
+    "IoGithubBtrfsbackupManager1Interface::[A-Z][A-Za-z0-9]+"
+    kde_signal_entries
+    "${kde_dbus_contract}"
+)
+set(kde_signal_names)
+foreach(entry IN LISTS kde_signal_entries)
+    string(REGEX REPLACE ".*Interface::([A-Za-z0-9]+)" "\\1" name "${entry}")
+    list(APPEND kde_signal_names "${name}")
+endforeach()
+assert_same_set(kde_signal_names contract_signal_names "KDE client signals")
+
+string(REGEX MATCHALL
+    "inline constexpr char [a-z0-9_]+\\[\\] = \"[A-Z][A-Za-z0-9]+\";"
+    protocol_member_entries
+    "${manager_protocol}"
+)
+set(protocol_member_names)
+foreach(entry IN LISTS protocol_member_entries)
+    string(REGEX REPLACE ".*= \"" "" name "${entry}")
+    string(REGEX REPLACE "\".*" "" name "${name}")
+    list(APPEND protocol_member_names "${name}")
+endforeach()
+set(contract_member_names ${contract_method_names} ${contract_signal_names})
+assert_same_set(protocol_member_names contract_member_names "protocol constants")
