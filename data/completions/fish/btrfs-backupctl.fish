6513131a8cda518f028d59fa699640255559d5d3
Kamil Piwowarski <kapiwko@gmail.com>
2026-09-05 20:15:06 +0200

    feat(cli): install man pages and shell completions

---
+++ data/completions/fish/btrfs-backupctl.fish
@@ -0,0 +1,7 @@
+set -l btrfs_backupctl_commands profile repository status installation runner restore target upgrade
+complete -c btrfs-backupctl -n 'not __fish_seen_subcommand_from $btrfs_backupctl_commands' -a "$btrfs_backupctl_commands"
+complete -c btrfs-backupctl -l status-root -r -d 'Override status root'
+complete -c btrfs-backupctl -l history-root -r -d 'Override history root'
+complete -c btrfs-backupctl -l profile-dir -r -d 'Override profile root'
+complete -c btrfs-backupctl -l profile -r -d 'Select profile'
+complete -c btrfs-backupctl -l help -s h -d 'Show help'
